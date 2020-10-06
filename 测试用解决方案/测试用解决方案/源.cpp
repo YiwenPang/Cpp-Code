9c6dd67536f33a7042f98acd8df7489e74a7fa71
Peter Pang <943368780@163.com>
2020-10-06 14:51:52 +0800

    我也不知道改了啥

--- 测试用解决方案/测试用解决方案/源.cpp
+++ 测试用解决方案/测试用解决方案/源.cpp
@@ -12,7 +12,7 @@ void init(linklist& l)
 {
 	l = new lnode;
 	l->next = NULL;
-	linklist p;
+	linklist p, temp=l;
 	cout << "元素个数：";
 	int m;
 	cin >> m;
@@ -21,8 +21,9 @@ void init(linklist& l)
 		p = new lnode;
 		cout << "输入数据：";
 		cin >> p->data;
-		p->next = l->next;
-		l->next = p;
+		temp->next = p;
+		p->next = NULL;
+		temp = temp->next;
 	}
 }
 int display(linklist l)
@@ -30,16 +31,45 @@ int display(linklist l)
 	if (!l)
 		return ERROR;
 	l = l->next;
-	while (l != NULL)
+	while (1)
 	{
 		cout << l->data << ",";
 		l = l->next;
+		if (l->next == NULL)
+		{
+			cout << l->data;
+			break;
+		}
 	}
 	return OK;
 }
+void jiehe(linklist& l1, linklist& l2)
+{
+	linklist pa, pb, pc, l3;
+	pa = l1->next;  pb = l2->next;
+	l3 = pc = l1;
+	while (pa && pb)
+	{
+		if (pa->data <= pb->data)
+		{
+			pc->next = pa; pc = pa;  pa = pa->next;
+		}
+		else
+		{
+			pc->next = pb;  pc = pb;  pb = pb->next;
+		}
+	}
+	pc->next = pa ? pa : pb;
+	delete  l2;
+	display(l3);
+}
 int main()
 {
-	linklist l;
-	init(l);
-	cout << display(l);
+	linklist l1, l2;
+	init(l1);
+	init(l2);
+	display(l1);
+	display(l2);
+	cout << "结合后为：";
+	jiehe(l1, l2);
 }
